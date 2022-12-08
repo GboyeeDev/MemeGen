d47bb914148b2f25ef3f4c1c4fff8323b5f375a5
Olugboyega <114001863+GboyeeDev@users.noreply.github.com>
2022-12-08 16:50:30 +0100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import '../style.css'
+
+
+function Header() {
+    return(
+        <header className="header">
+            <img src="../images/dev.jpg" className="header-image" />
+            <h3 className="header-title">Meme Generator</h3>
+            <h4 className="header-project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+
+export default Header;
